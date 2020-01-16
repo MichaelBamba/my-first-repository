1ce4fdbbb0392d7e280e38c65ef2f38aec916373
MichaelBamba <mjbamba2@gmail.com>
2020-01-16 08:44:08 -0500

    working timer how i want it basic

--- pythonICE/timer.py
+++ pythonICE/timer.py
@@ -1,9 +1,14 @@
 import time
-n = time
+
+how_long = int(input("How many minutes would you like time"))
+settimmer = (how_long * 60)
+
 def countdown(t):
     while t > 0:
         print (t)
         t = t -1
+        time.sleep(1)
     if t ==0:
         print ("times up")
-countdown(500000000)
+    
+countdown( settimmer )
